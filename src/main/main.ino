f004a7eb1faaa70fad2571395510d62b87b178f0
ymt117 <yamamto117@gmail.com>
2019-05-07 23:12:14 +0900

    Update main.ino

--- src/main/main.ino
+++ src/main/main.ino
@@ -2,7 +2,7 @@
  * ARLISS2019 CanSat
  * 
  * Author: ymt
- * Secsor: LSM6, LPS, LIS3MDL, GPS
+ * Sensor: LSM6, LPS, LIS3MDL, GPS
  */
 
 #include "spdef.h"
@@ -19,11 +19,17 @@ void setup() {
 
   pwm_init();
 
+  // Set button
+  pinMode(PA4, INPUT_PULLUP);
+
   beep(BOOT_UP);
   delay(100);
 }
 
 void loop() {
+  if(!digitalRead(PA4)){
+    beep(SHUT_DOWN);
+  }
   imu_read();
   gps_read();
   pwm_test();
