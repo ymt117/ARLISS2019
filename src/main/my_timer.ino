677afb11a609850d036680087ecc7bd86bb2d8dd
ymt117 <yamamto117@gmail.com>
2019-07-07 01:20:06 +0900

    Update Timer interrupt function
    タイマー割込み処理を更新
    スイッチ長押しで終了処理に移行するプログラムにした

--- src/main/my_timer.ino
+++ src/main/my_timer.ino
@@ -3,16 +3,32 @@
 // Handler for stimer
 static stimer_t TimHandle;
 
-static void _sw_counter_reset(stimer_t *htim){
+volatile uint8_t press_old_state = 0;
+volatile uint16_t press_time = 0;
+
+static void _exit(stimer_t *htim){
   UNUSED(htim);
-  sw_counter = 0;
-  digitalWrite(led2, !digitalRead(led2));
+  // Count while button is pressed.
+  if(!digitalRead(sw) && press_old_state == 1){
+    press_time++;
+    //digitalWrite(led2, !digitalRead(led2));
+    //Serial.println(press_time);
+    if(press_time > 50) s = State_exit;
+  }
+  else if(!digitalRead(sw)){
+    press_old_state = 1;
+  }
+  // Reset count if button is not pressed.
+  else{
+    press_old_state = 0;
+    press_time = 0;
+  }
 }
 
 void timer_init(){
   // Set TIMx instance.
   TimHandle.timer = MY_TIMER_EXIT;
-  // Timer set to 10ms
+  // Timer set to 10ms=100000?
   TimerHandleInit(&TimHandle, 100000 - 1, ((uint32_t)(getTimerClkFreq(MY_TIMER_EXIT) / (1000000)) - 1));
-  attachIntHandle(&TimHandle, _sw_counter_reset);
+  attachIntHandle(&TimHandle, _exit);
 }
