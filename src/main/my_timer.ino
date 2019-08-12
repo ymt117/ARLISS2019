4c528e2bb19e290046fa87b706e887e2e34104dd
ymt117 <yamamto117@gmail.com>
2019-08-13 08:47:08 +0900

    Update my_timer.ino

--- src/main/my_timer.ino
+++ src/main/my_timer.ino
@@ -1,19 +1,53 @@
-#define MY_TIMER TIM4
+#define MY_TIMER_3 TIM3
+#define MY_TIMER_4 TIM4
 
 // Handler for stimer
-static stimer_t TimHandle;
+static stimer_t TimHandle_3;
+static stimer_t TimHandle_4;
 
 volatile int my_timer_counter = 0;
+volatile long old_millis_3 = 0;
+volatile long old_millis_4 = 0;
 
-static void _call_func(stimer_t *htim){
+/***************************************************************
+ * Function called by timer interrupt
+ **************************************************************/
+static void _call_func_3(stimer_t *htim){
   UNUSED(htim);
-  madgwick_update();
+
+  my_timer_counter++;
+  if(my_timer_counter >= 5){
+    Serial.print("Timer 3 called: "); Serial.println(millis() - old_millis_3);
+    old_millis_3 = millis();
+    my_timer_counter = 0;
+  }
 }
 
-void timer_init(){
+static void _call_func_4(stimer_t *htim){
+  UNUSED(htim);
+  madgwick_update();
+  Serial.print("Timer 4 called: "); Serial.println(millis() - old_millis_4);
+  old_millis_4 = millis();
+}
+
+/***************************************************************
+ * TIM 3 Initialization
+ **************************************************************/
+void timer_init_3(){
   // Set TIMx instance.
-  TimHandle.timer = MY_TIMER;
+  TimHandle_3.timer = MY_TIMER_3;
   // Timer set to 10ms=10000
-  TimerHandleInit(&TimHandle, 50000 - 1, ((uint32_t)(getTimerClkFreq(MY_TIMER) / (1000000)) - 1));
-  attachIntHandle(&TimHandle, _call_func);
+  TimerHandleInit(&TimHandle_3, 50000 - 1, ((uint32_t)(getTimerClkFreq(MY_TIMER_3) / (1000000)) - 1));
+  attachIntHandle(&TimHandle_3, _call_func_3);
+}
+
+/***************************************************************
+ * TIM 4 Initialization
+ **************************************************************/
+void timer_init_4(){
+  // Set TIMx instance.
+  TimHandle_4.timer = MY_TIMER_4;
+  // Timer set to 10ms=10000
+  TimerHandleInit(&TimHandle_4, 50000 - 1, ((uint32_t)(getTimerClkFreq(MY_TIMER_4) / (1000000)) - 1));
+  attachIntHandle(&TimHandle_4, _call_func_4);
 }
