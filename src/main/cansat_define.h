86d3962bb1370a97e161f3b2b7a5a0b6db5831b7
ymt117 <yamamto117@gmail.com>
2019-08-20 14:09:12 +0900

    Update

--- src/main/cansat_define.h
+++ src/main/cansat_define.h
@@ -24,15 +24,6 @@ MyState s = State_init;
 /***************************************************************
  * Speaker
  **************************************************************/
-#define mC 261.626
-#define mD 293.665
-#define mE 329.628
-#define mF 349.228
-#define mG 391.995
-#define mA 440.000
-#define mB 493.883
-#define nn 0.0
-
 #define BOOT_UP       1
 #define SHUT_DOWN     2
 #define CLANNAD       3
--- src/main/speaker.ino
+++ src/main/speaker.ino
@@ -2,6 +2,15 @@
 
 //#define BEEP_OFF
 
+#define mC 261.626
+#define mD 293.665
+#define mE 329.628
+#define mF 349.228
+#define mG 391.995
+#define mA 440.000
+#define mB 493.883
+#define nn 0.0
+
 // BOOT_UP
 float mm1[] = {mE*4, mA*4, mB*4, mA*4, mE*2, mE*4, mB*4};
 
