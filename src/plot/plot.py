6b23405110b446a1c8ecd5a9632138a7e80889a1
ymt117 <yamamto117@gmail.com>
2019-09-04 22:41:11 +0900

    Update plot.py

--- src/plot/plot.py
+++ src/plot/plot.py
@@ -6,8 +6,8 @@ import math
 
 def plot_lat_lng():
     # GOALの緯度・経度
-    g_lat = 31.567613
-    g_lng = 130.543375
+    g_lat = 31.567389
+    g_lng = 130.543038
 
     # csvファイルを読み込む
     # データは3列で構成される
@@ -22,14 +22,23 @@ def plot_lat_lng():
     # 軸の下限と上限を設定するために最大値と最小値を所得する
     lat_max, lng_max, _ = df.max(axis='index')
     lat_min, lng_min, _ = df.min(axis='index')
+    if lat_max < g_lat:
+        lat_max = g_lat
+    if lat_min > g_lat:
+        lat_min = g_lat
+    if lng_max < g_lng:
+        lng_max = g_lng
+    if lng_min > g_lng:
+        lng_min = g_lng
 
     # 軸の下限と上限を設定する
     # x軸は経度，y軸は緯度
-    ax.set_xlim(lng_min-0.0002, lng_max+0.0002)
-    ax.set_ylim(lat_min-0.0002, lat_max+0.0002)
+    ax.set_xlim(lng_min-0.0004, lng_max+0.0004)
+    ax.set_ylim(lat_min-0.0004, lat_max+0.0004)
 
     # 矢印を描画する
     for i in range(len(df)):
+        # 矢印の終了点を計算する
         end_x = (math.cos(df['control'][i]*0.01745329252)*0.00005 + df['lng'][i]) # cosθ + 経度
         end_y = (math.sin(df['control'][i]*0.01745329252)*0.00005 + df['lat'][i]) # sinθ + 緯度
 
@@ -38,14 +47,17 @@ def plot_lat_lng():
             'start': [df['lng'][i], df['lat'][i]],
             'end':   [end_x,        end_y]
         }
+        r = math.sqrt(((end_x - df['lng'][i]) * (end_x - df['lng'][i])) + ((end_y - df['lat'][i]) * (end_y - df['lat'][i])))
+        print(r)
 
         # 矢印の設定
         if i == 0:
-            plot_str = "  Control start point\n  "+str(df['lat'][i])+"\n  "+str(df['lng'][i])
+            plot_str = ""#  Control start point\n  "+str(round(df['lat'][i], 6))+"\n  "+str(round(df['lng'][i], 6))
         elif i == len(df)-1:
-            plot_str = "  Control finish point\n  "+str(df['lat'][i])+"\n  "+str(df['lng'][i])
+            plot_str = ""#  Control finish point\n  "+str(round((df['lat'][i]), 6))+"\n  "+str(round(df['lng'][i], 6))
         else:
             plot_str = ""
+
         ax.annotate(plot_str, xy=point['end'], xytext=point['start'],
                     arrowprops=dict(shrink=0, width=1, headwidth=8,
                                     headlength=10, connectionstyle='arc3',
@@ -54,8 +66,11 @@ def plot_lat_lng():
 
     # 折れ線を描画する
     plt.plot(g_lng, g_lat, '^')
-    ax.annotate("  Target point\n  "+str(g_lat)+"\n  "+str(g_lng), (g_lng, g_lat))
+    ax.annotate("  Target point\n  "+str(round(g_lat, 6))+"\n  "+str(round(g_lng, 6)), (g_lng, g_lat))
     plt.plot(df['lng'], df['lat'], '-o', linestyle='dashed', color='black')
+
+    plt.plot(df['lng'][0], df['lat'][0], '^', color='orange')
+    plt.plot(df['lng'][len(df)-1], df['lat'][len(df)-1], '^', color='green')
     plt.xlabel('longtitude')
     plt.ylabel('latitude')
     plt.show()
